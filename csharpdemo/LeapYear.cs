7ab78181154f67aa1357f566e2f992c2108206f9
srikanthpragada <srikanthpragada@gmail.com>
2021-03-05 09:35:10 +0530

    05-MAR

---
+++ csharpdemo/LeapYear.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace csharpdemo
+{
+    class LeapYear
+    {
+        static void Main(string[] args)
+        {
+            int year;
+
+            Console.Write("Enter year :");
+            year = Int32.Parse(Console.ReadLine());
+
+            if (year % 4 == 0)
+                Console.WriteLine($"{year} is a leap year");
+            else
+                Console.WriteLine($"{year} is not a leap year");
+        }
+    }
+}
---
+++ csharpdemo/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace csharpdemo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
